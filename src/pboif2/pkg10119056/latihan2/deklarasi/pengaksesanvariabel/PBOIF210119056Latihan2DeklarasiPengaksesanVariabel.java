1fc17c2d38cdb1cd79e8db6c035b57623324b206
ACER <ACER@Irfan>
2020-10-16 18:42:06 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan2/deklarasi/pengaksesanvariabel/PBOIF210119056Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan2.deklarasi.pengaksesanvariabel;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan  Pengaksesan Variabel
+ */
+public class PBOIF210119056Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Dekklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt=78;
+        nilaiLogika=false;
+        nilaiKarakter='D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai =" + nilaiInt);
+        System.out.println("Isi variabel PHI =" + PHI);
+        System.out.println("Isi variabel logika =" + nilaiLogika);
+        System.out.println("Isi variabel karakter =" + nilaiKarakter);
+        
+    }
+    
+}
